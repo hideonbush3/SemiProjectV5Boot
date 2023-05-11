3f0bf65d8cc45da1b1a7dd325c6634237e7b20f3
hideonbush3 <ppoii0961@naver.com>
2023-05-11 12:57:50 +0900

    저장소 초기화 - boot 프로젝트 설정

---
+++ src/main/java/hideonbush/springboot/semiproject5v/SemiProjectV5Application.java
@@ -0,0 +1,13 @@
+package hideonbush.springboot.semiproject5v;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemiProjectV5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SemiProjectV5Application.class, args);
+    }
+
+}
